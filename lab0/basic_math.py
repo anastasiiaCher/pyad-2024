1c0f278d9b91ac83f9981ad7ccc2906af1d83e19
Vladislav Bordiug <vladislav.bordioug@gmail.com>
2024-10-14 00:43:33 +0300

    Update basic_math.py

--- lab0/basic_math.py
+++ lab0/basic_math.py
@@ -7,7 +7,7 @@ def matrix_multiplication(m1, m2):
     for i in range(len(m1)):
         for j in range(len(m2[0])):
             for k in range(len(m1[0])):
-            ans[i][j] += m1[i][k]*m2[k][j]
+                ans[i][j] += m1[i][k]*m2[k][j]
     return ans
 
 
