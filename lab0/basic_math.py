ca923b528bf712fb984ca390d221fd152e6a539e
Korchagin Vadim Sergeevich <89642064+qerenny@users.noreply.github.com>
2024-10-16 20:15:51 +0300

    done

--- lab0/basic_math.py
+++ lab0/basic_math.py
@@ -1,39 +1,72 @@
 import numpy as np
-import scipy as sc
+
+def matrix_multiplication(matrix1, matrix2):
+    rows_matrix1 = len(matrix1)
+    cols_matrix1 = len(matrix1[0])
+    rows_matrix2 = len(matrix2)
+    cols_matrix2 = len(matrix2[0])
+
+    if cols_matrix1 != rows_matrix2:
+        raise ValueError("Матрицы нельзя перемножить.")
+
+    matrix3 = [[0 for _ in range(cols_matrix2)] for _ in range(rows_matrix1)]
+
+    for i in range(rows_matrix1):
+        for j in range(cols_matrix2):
+            for k in range(cols_matrix1):
+                matrix3[i][j] += matrix1[i][k] * matrix2[k][j]
+
+    return matrix3
 
 
-def matrix_multiplication(matrix_a, matrix_b):
-    """
-    Задание 1. Функция для перемножения матриц с помощью списков и циклов.
-    Вернуть нужно матрицу в формате списка.
-    """
-    # put your code here
-    pass
 
 
-def functions(a_1, a_2):
-    """
-    Задание 2. На вход поступает две строки, содержащие коэффициенты двух функций.
-    Необходимо найти точки экстремума функции и определить, есть ли у функций общие решения.
-    Вернуть нужно координаты найденных решения списком, если они есть. None, если их бесконечно много.
-    """
-    # put your code here
-    pass
+def functions(coeffs1, coeffs2):
+    a1 = list(map(float, coeffs1.split()))
+    a2 = list(map(float, coeffs2.split()))
+
+    a_diff = [a1[i] - a2[i] for i in range(3)]
+
+    if np.all(np.isclose(a_diff, 0)):
+        return None
+
+    if a_diff[0] == 0:
+        if a_diff[1] == 0:
+            return []
+        x = -a_diff[2] / a_diff[1]
+        return [(round(x, 6), round(a1[0]*x**2 + a1[1]*x + a1[2], 6))]
+
+    discriminant = a_diff[1]**2 - 4*a_diff[0]*a_diff[2]
+
+    if discriminant > 0:
+        x1 = (-a_diff[1] + np.sqrt(discriminant)) / (2 * a_diff[0])
+        x2 = (-a_diff[1] - np.sqrt(discriminant)) / (2 * a_diff[0])
+        return [
+            (round(x1, 6), round(a1[0]*x1**2 + a1[1]*x1 + a1[2], 6)),
+            (round(x2, 6), round(a1[0]*x2**2 + a1[1]*x2 + a1[2], 6))
+        ]
+    elif np.isclose(discriminant, 0):
+        x = -a_diff[1] / (2 * a_diff[0])
+        return [(round(x, 6), round(a1[0]*x**2 + a1[1]*x + a1[2], 6))]
+    else:
+        return []
 
 
 def skew(x):
-    """
-    Задание 3. Функция для расчета коэффициента асимметрии.
-    Необходимо вернуть значение коэффициента асимметрии, округленное до 2 знаков после запятой.
-    """
-    # put your code here
-    pass
+    mean = np.mean(x)
+    m2 = np.mean((x - mean) ** 2)
+    m3 = np.mean((x - mean) ** 3)
+    sigma = np.sqrt(m2)
+    skewness = m3 / sigma ** 3
+    return round(skewness, 2)
 
 
 def kurtosis(x):
-    """
-    Задание 3. Функция для расчета коэффициента эксцесса.
-    Необходимо вернуть значение коэффициента эксцесса, округленное до 2 знаков после запятой.
-    """
-    # put your code here
-    pass
+    mean = np.mean(x)
+    m2 = np.mean((x - mean) ** 2)
+    m4 = np.mean((x - mean) ** 4)
+    sigma = np.sqrt(m2)
+    kurt = m4 / sigma ** 4 - 3
+    return round(kurt, 2)
+
+
