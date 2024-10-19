c18cd890a6ff5dfc7f591196595b1ef0a30bda13
UlitiM2 <113083737+UlitiM2@users.noreply.github.com>
2024-10-19 17:43:58 +0300

    Update basic_math.py

--- lab0/basic_math.py
+++ lab0/basic_math.py
@@ -7,18 +7,17 @@ def matrix_multiplication(matrix_a, matrix_b):
     Задание 1. Функция для перемножения матриц с помощью списков и циклов.
     Вернуть нужно матрицу в формате списка.
     """
-    m = len(matrix1)
-    n = len(matrix2)
-    p = len(matrix2[0])
-    c = [[None for d in range(p)] for d in range(m)]
-
-    if len(matrix1[0]) == len(matrix2):
-      for i in range(m):
-        for j in range(p):
-            c[i][j] = sum(matrix1[i][f] * matrix2[f][j] for f in range(n))
-      print ("Произведенеие матриц:", c)
+        if len(matrix_a[0]) == len(matrix_b):
+        c = []
+        for _ in range(len(matrix_a)):
+            c += [[0]*len(matrix_b[0])]
+        for i in range(len(matrix_a)):
+            for j in range(len(matrix_b[0])):
+                for f in range(len(matrix_b)):
+                    c[i][j] += matrix_a[i][f] * matrix_b[f][j]
+        return c
     else:
-      print ("Матрицы невозможно перемножить")
+        raise ValueError ("Матрицы невозможно перемножить")
     pass
 
 
@@ -28,41 +27,31 @@ def functions(a_1, a_2):
     Необходимо найти точки экстремума функции и определить, есть ли у функций общие решения.
     Вернуть нужно координаты найденных решения списком, если они есть. None, если их бесконечно много.
     """
-    def F(x, a):
-        return a[0] * x**2 + a[1] * x + a[2]
-    def P(x, b):
-        return b[0] * x**2 + b[1] * x + b[2]
-    def find_extremum(F, a):
-        x_ext = -a[1] / (2 * a[0])
-        extremum_value = F(x_ext, a)
-        return x_ext, extremum_value
-    def find_common_solutions(a, b):
-        coeffs = [a[0] - b[0], a[1] - b[1], a[2] - b[2]]
-        if coeffs[0] == 0 and coeffs[1] == 0 and coeffs[2] == 0:
-            return "Бесконечно много решений"
-        if coeffs[0] == 0:
-            if coeffs[1] == 0:
-                return "Нет решений"
-            return f"Одно решение: x = {-coeffs[2] / coeffs[1]}"
-        D = coeffs[1]**2 - 4 * coeffs[0] * coeffs[2]
-        if D < 0:
-            return "Нет решений"
-        elif D == 0:
-            x = -coeffs[1] / (2 * coeffs[0])
-            return f"Одно решение: x = {x}"
-        else:
-            x1 = (-coeffs[1] + D**0.5) / (2 * coeffs[0])
-            x2 = (-coeffs[1] - D**0.5) / (2 * coeffs[0])
-            return f"Два решения: x1 = {x1}, x2 = {x2}"
-    
-    line1 = input("Введите коэффициенты для F(x): ")
-    line2 = input("Введите коэффициенты для P(x): ")
-    extremum_F = find_extremum(F, a)
-    extremum_P = find_extremum(P, b)
-    common_solutions = find_common_solutions(a, b)
-    print(f"Экстремум F(x) в точке x = {extremum_F[0]}, значение F(x) = {extremum_F[1]}")
-    print(f"Экстремум P(x) в точке x = {extremum_P[0]}, значение P(x) = {extremum_P[1]}")
-    print(f'Общие решения:{common_solutions}')
+    if a_1 == a_2:
+        return None
+    a11, a12, a13 = map(float, a_1.split())
+    a21, a22, a23 = map(float, a_2.split())
+    def F(x):
+      return a11 * x ** 2 + a12 * x + a13
+    if a11 != 0:
+        x_extr_1 = (-a12) / (2 * a11)
+    if a21 != 0:
+        x_extr_2 = (-a22) / (2 * a21)
+    res = []
+    a = a11 - a21
+    b = a12 - a22
+    c = a13 - a23
+    d = b**2 - 4*a*c
+    if a == 0:
+        if b != 0:
+            x = -c / b
+            res.append((x, F(x)))
+    elif d > 0:
+        x1 = (-b + (d)**0.5) / (2*a)
+        x2 = (-b - (d)**0.5) / (2*a)
+        res.append((x1, F(x1)))
+        res.append((x2, F(x2)))
+    return res
     pass
 
 
@@ -71,25 +60,17 @@ def skew(x):
     Задание 3. Функция для расчета коэффициента асимметрии.
     Необходимо вернуть значение коэффициента асимметрии, округленное до 2 знаков после запятой.
     """
-    sr = sum(m) / len(m)
-    def disp(m, mean, x):
-      mom = 0
-      for j in range(len(m)):
-        mom += (m[j]-mean) ** x
-      mom = mom/len(m)
-      return mom
-    
-    def res(m, m2,m3,m4):
-      A3 = m3/(m2**3**0.5)
-      E4 = m4/(m2**4**0.5)-3
-      return A3
-    
-    m2 = disp(m, sr, 2)
-    m3 = disp(m, sr, 3)
-    m4 = disp(m, sr, 4)
-    res = res(m, m2, m3, m4)
-    print(f'Среднее {sr}\n m2 {m2}\n m3 {m3}\n m4 {m4}\n Коэффициенты асимметрии и эксцесса: {res}')
-
+    m3 = 0
+    d2 = 0
+    n = len(x)
+    e = sum(x) / n
+    for i in x:
+        m3 += (i - e) ** 3
+        d2 += (i - e) ** 2
+    m3 = m3 / n
+    d2 = d2 / n
+    A = m3 / (d2 ** (3 / 2))
+    return round(A, 2)
     pass
 
 
@@ -98,22 +79,15 @@ def kurtosis(x):
     Задание 3. Функция для расчета коэффициента эксцесса.
     Необходимо вернуть значение коэффициента эксцесса, округленное до 2 знаков после запятой.
     """
-    sr = sum(m) / len(m)
-    def disp(m, mean, x):
-      mom = 0
-      for j in range(len(m)):
-        mom += (m[j]-mean) ** x
-      mom = mom/len(m)
-      return mom
-    
-    def res(m, m2,m3,m4):
-      A3 = m3/(m2**3**0.5)
-      E4 = m4/(m2**4**0.5)-3
-      return E4
-    
-    m2 = disp(m, sr, 2)
-    m3 = disp(m, sr, 3)
-    m4 = disp(m, sr, 4)
-    res = res(m, m2, m3, m4)
-    print(f'Среднее {sr}\n m2 {m2}\n m3 {m3}\n m4 {m4}\n Коэффициенты асимметрии и эксцесса: {res}')
+    m4 = 0
+    d2 = 0
+    n = len(x)
+    e = sum(x) / n
+    for i in x:
+        m4 += (i - e) ** 4
+        d2 += (i - e) ** 2
+    m4 = m4 / n
+    d2 = d2 / n
+    E = (m4 / (d2**2))-3
+    return round(E, 2)
     pass
