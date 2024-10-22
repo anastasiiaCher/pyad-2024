ae821fe0a9e96c40266bf0f9c37c06d660b5e61d
MariamNaderi <112973343+MariamNaderi@users.noreply.github.com>
2024-10-22 23:06:56 +0300

    Add files via upload

--- lab0/basic_math.py
+++ lab0/basic_math.py
@@ -9,9 +9,9 @@ def matrix_multiplication(matrix_a, matrix_b):
     Вернуть нужно матрицу в формате списка.
     """
     # put your code here
-    if matrix_a.shape[1] != matrix_b.shape[0]:
-      return 'Данные матрицы нельзя перемножить'
-    n, m, k = matrix_a.shape[0], matrix_b.shape[0], matrix_b.shape[1]
+    if len(matrix_a[0]) != len(matrix_b):
+        raise ValueError
+    n, m, k = len(matrix_a), len(matrix_b), len(matrix_b[0])
     matrix = np.zeros((n, k), dtype=int)
 
     for i in range(n):
@@ -19,7 +19,7 @@ def matrix_multiplication(matrix_a, matrix_b):
             for p in range(m):
                 matrix[i][j] += matrix_a[i][p]*matrix_b[p][j]
 
-    return matrix
+    return matrix.tolist()
 
 
 def functions(a_1, a_2):
@@ -29,9 +29,15 @@ def functions(a_1, a_2):
     Вернуть нужно координаты найденных решения списком, если они есть. None, если их бесконечно много.
     """
     # put your code here
+    a_1 = [int(i) for i in a_1.split()]
+    a_2 = [int(i) for i in a_2.split()]
     if a_2 == a_1:
-        return 'Бесконечное множество решений'
+        return None
     D = (a_1[1]-a_2[1])**2 - 4*(a_1[0]-a_2[0])*(a_1[2]-a_2[2])
+    if (a_1[0]-a_2[0]) == 0:
+        if (a_1[1]-a_2[1]) == 0: return []
+        x1 = (-a_1[2]+a_2[2]) / (a_1[1]-a_2[1])
+        return [(round(x1, 3), round(a_1[0]*x1*x1+a_1[1]*x1+a_1[2], 3))]
     if D > 0:
         x1 = ((-a_1[1]+a_2[1]) + D**0.5)/(2*((a_1[0]-a_2[0])))
         x2 = ((-a_1[1]+a_2[1]) - D**0.5)/(2*((a_1[0]-a_2[0])))
@@ -41,7 +47,7 @@ def functions(a_1, a_2):
         x2 = x1
         return [(round(x1, 3), round(a_1[0]*x1*x1+a_1[1]*x1+a_1[2], 3))]
     else:
-        return 'Нет общих решений
+        return []
 
 
 def fun(X):
@@ -76,4 +82,3 @@ def kurtosis(x):
     """
     # put your code here
     return round(fun(x)[1], 2)
-
