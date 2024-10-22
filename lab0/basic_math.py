b217edd53a4a689856e272a7e6f831b3a177c87f
AgutinVBoy <68972811+AgutinVBoy@users.noreply.github.com>
2024-10-22 13:53:50 +0300

    Выполнена задача 2

--- lab0/basic_math.py
+++ lab0/basic_math.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.polynomial.polynomial import Polynomial
 import scipy as sc
 
 
@@ -24,8 +25,31 @@ def functions(a_1, a_2):
     Необходимо найти точки экстремума функции и определить, есть ли у функций общие решения.
     Вернуть нужно координаты найденных решения списком, если они есть. None, если их бесконечно много.
     """
-    # put your code here
-    pass
+    def parse_params(params: str):
+        return [float(p) for p in params.split()]
+
+    def func(params: list[float]):
+        return lambda x: params[0] * x ** 2 + params[1] * x + params[2]
+
+    a1 = parse_params(a_1)
+    a2 = parse_params(a_2)
+    params_difference = [a1[i] - a2[i] for i in range(len(a1))]
+
+    if params_difference == [0, 0, 0]:
+        return None
+
+    params_difference.reverse()
+    p = Polynomial(params_difference)
+    intersection_x = p.roots()
+
+    real_roots = [x.real for x in intersection_x if x.imag == 0]
+    if not real_roots:
+        return []
+
+    f1 = func(a1)
+    intersection_y = [f1(x) for x in real_roots]
+
+    return list(zip(real_roots, intersection_y))
 
 
 def skew(x):
