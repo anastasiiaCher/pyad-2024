0783ed42580b760cf7c35c6ac39d677b3a0dbbe0
Mariam-Guseinova <mariam.guseinova.itmo@gmail.com>
2024-10-23 19:50:29 +0300

    feat: 1 & 3 tasks

--- lab0/basic_math.py
+++ lab0/basic_math.py
@@ -1,15 +1,23 @@
-import numpy as np
-import scipy as sc
-
-
+# Задание №1
 def matrix_multiplication(matrix_a, matrix_b):
-    """
-    Задание 1. Функция для перемножения матриц с помощью списков и циклов.
-    Вернуть нужно матрицу в формате списка.
-    """
-    # put your code here
-    pass
+    # Проверяем возможность умножения: число столбцов matrix1 должно быть равно числу строк matrix2
+    if len(matrix_a[0]) != len(matrix_b):
+        raise ValueError("Матрицы не могут быть перемножены: размерности не соответствуют.")
 
+    result = [[0] * len(matrix_b[0]) for _ in range(len(matrix_a))]
+
+    # Проход по строкам matrix1
+    for i in range(len(matrix_a)):
+        # Проход по столбцам matrix2
+        for k in range(len(matrix_b[0])):
+            # Вычисление каждого элемента новой матрицы
+            for j in range(len(matrix_b)):
+                result[i][k] += matrix_a[i][j] * matrix_b[j][k]
+
+    return result
+
+
+# Задание №2
 
 def functions(a_1, a_2):
     """
@@ -21,19 +29,22 @@ def functions(a_1, a_2):
     pass
 
 
+# Задание №3
 def skew(x):
-    """
-    Задание 3. Функция для расчета коэффициента асимметрии.
-    Необходимо вернуть значение коэффициента асимметрии, округленное до 2 знаков после запятой.
-    """
-    # put your code here
-    pass
+    if len(x) == 0:
+        return None  # Возвращаем None для пустой выборки
+    mean = sum(x) / len(x)
+    m2 = sum((num - mean) ** 2 for num in x) / len(x)  # Момент второго порядка
+    m3 = sum((num - mean) ** 3 for num in x) / len(x)  # Момент третьего порядка
+    skewness = m3 / (m2 ** (3 / 2)) if m2 != 0 else 0  # Проверка на ноль в знаменателе
+    return round(skewness, 2)  # Округляем до 2 знаков после запятой
 
 
 def kurtosis(x):
-    """
-    Задание 3. Функция для расчета коэффициента эксцесса.
-    Необходимо вернуть значение коэффициента эксцесса, округленное до 2 знаков после запятой.
-    """
-    # put your code here
-    pass
+    if len(x) == 0:
+        return None  # Возвращаем None для пустой выборки
+    mean = sum(x) / len(x)
+    m2 = sum((num - mean) ** 2 for num in x) / len(x)  # Момент второго порядка
+    m4 = sum((num - mean) ** 4 for num in x) / len(x)  # Момент четвертого порядка
+    kurt = m4 / (m2 ** 2) - 3 if m2 != 0 else 0  # Проверка на ноль в знаменателе
+    return round(kurt, 2)  # Округляем до 2 знаков после запятой
