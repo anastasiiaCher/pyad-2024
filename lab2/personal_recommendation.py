e62f1e227204822c42ac2110ef4b04691091b188
Ekarerina-Ulyanova <113061011+Ekarerina-Ulyanova@users.noreply.github.com>
2024-12-22 19:12:44 +0300

    Update personal_recommendation.py

--- lab2/personal_recommendation.py
+++ lab2/personal_recommendation.py
@@ -1,1 +1,91 @@
+Book: The Very Best Baby Name Book in the Whole Wide World
+Predicted rating: 8.05
+LinReg rating: 48.19
 
+Book: Lonesome Dove
+Predicted rating: 8.36
+LinReg rating: 7.65
+
+Book: The Talisman
+Predicted rating: 8.11
+LinReg rating: 4.14
+
+Book: Persuasion
+Predicted rating: 8.02
+LinReg rating: 4.11
+
+Book: Into Thin Air : A Personal Account of the Mount Everest Disaster
+Predicted rating: 8.03
+LinReg rating: 1.07
+
+Book: The Lion, the Witch and the Wardrobe (Full-Color Collector's Edition)
+Predicted rating: 8.17
+LinReg rating: 0.94
+
+Book: The Magician's Nephew (Narnia)
+Predicted rating: 8.36
+LinReg rating: 0.78
+
+Book: October Sky: A Memoir
+Predicted rating: 8.10
+LinReg rating: -1.56
+
+Book: A Prayer for Owen Meany
+Predicted rating: 8.11
+LinReg rating: -1.75
+
+Book: The Magician's Nephew (rack) (Narnia)
+Predicted rating: 8.06
+LinReg rating: -4.08
+
+Book: The Phantom Tollbooth
+Predicted rating: 8.03
+LinReg rating: -6.13
+
+Book: The Hobbit : The Enchanting Prelude to The Lord of the Rings
+Predicted rating: 8.20
+LinReg rating: -7.25
+
+Book: Harry Potter and the Sorcerer's Stone (Harry Potter (Paperback))
+Predicted rating: 8.02
+LinReg rating: -24.91
+
+Book: Yukon Ho!
+Predicted rating: 8.04
+LinReg rating: -26.83
+
+Book: Goodnight Moon Board Book
+Predicted rating: 8.18
+LinReg rating: -33.96
+
+Book: A Wrinkle In Time
+Predicted rating: 8.16
+LinReg rating: -49.20
+
+Book: Cat in the Hat (I Can Read It All by Myself Beginner Books)
+Predicted rating: 8.03
+LinReg rating: -62.18
+
+Book: Charlotte's Web (Trophy Newbery)
+Predicted rating: 8.23
+LinReg rating: -64.04
+
+Book: She Said Yes : The Unlikely Martyrdom of Cassie Bernall
+Predicted rating: 8.24
+LinReg rating: -106.14
+
+Book: Master and Commander (Aubrey-Maturin (Paperback))
+Predicted rating: 8.14
+LinReg rating: -125.57
+
+Book: Walden and Other Writings
+Predicted rating: 8.02
+LinReg rating: -147.86
+
+Book: Little House in the Big Woods
+Predicted rating: 8.11
+LinReg rating: -170.74
+
+Book: The Monster at the End of This Book
+Predicted rating: 8.24
+LinReg rating: -258.96
