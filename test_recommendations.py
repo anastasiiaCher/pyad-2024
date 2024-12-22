3c9383d172d8d572ae7cd6834c611ad71743eec2
danoneeee <90916110+danoneeee@users.noreply.github.com>
2024-12-23 00:09:42 +0300

    Add files via upload

---
+++ test_recommendations.py
@@ -0,0 +1,26 @@
+import pickle
+import pandas as pd
+import unittest
+from surprise import accuracy
+from sklearn.metrics import mean_absolute_error
+
+
+class RecsTestCase(unittest.TestCase):
+    def test_svd_recs(self):
+        with open('svd.pkl', 'rb') as f:
+            loaded_svd = pickle.load(f)
+        td = pd.read_csv("svd_test.csv")
+        predictions = loaded_svd.test(td.values)
+        mae = accuracy.mae(predictions)
+        self.assertLessEqual(mae, 1.3)
+
+
+    def test_linreg_recs(self):
+        with open('linreg.pkl', 'rb') as f:
+            loaded_linreg = pickle.load(f)
+        td = pd.read_csv("linreg_test.csv")
+        y = td.pop("y")
+        selected_columns = td.iloc[:, :8]
+        predictions = loaded_linreg.predict(selected_columns)
+        mae = mean_absolute_error(y, predictions)
+        self.assertLessEqual(mae, 1.5)
