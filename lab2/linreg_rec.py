a97b96164b1cdbe38e3779b9bff649eb7e691c9a
Анастасия Бакланова <112978018+Anasstasssia@users.noreply.github.com>
2024-12-23 23:19:37 +0300

    linreg_rec.py

--- lab2/linreg_rec.py
+++ lab2/linreg_rec.py
@@ -3,17 +3,27 @@ import re
 import nltk
 import pandas as pd
 import sklearn
-
+from sklearn.preprocessing import LabelEncoder
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import SGDRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
 
 nltk.download("stopwords")
 nltk.download("punkt")
 
+def fix_merged_columns(row):
+    merged_value = row['Book-Title']
+    pattern = r'^(.+?)\\";(.+)"$'
+    match = re.match(pattern, merged_value)
+    if match:
+        author, title = match.groups()
+        return author.strip(), title.strip()
+    else:
+        return None, None
 
 def books_preprocessing(df: pd.DataFrame) -> pd.DataFrame:
     """Функция для предобработки таблицы Books.scv"""
@@ -23,23 +33,18 @@ def books_preprocessing(df: pd.DataFrame) -> pd.DataFrame:
     df['Year-Of-Publication'] = df['Year-Of-Publication'].astype(str)
     mask = ~df["Year-Of-Publication"].str.contains(r'\D')
     books_filtered = df[mask]
-    
-    # Применяем функцию исправления данных
-    fixed_df = new_sort.apply(lambda x: fix_merged_columns(x), axis=1)
-    new_sort['Publisher'] = new_sort['Year-Of-Publication']
-    new_sort['Year-Of-Publication'] = new_sort['Book-Author']
-    new_sort[['Book-Title', 'Book-Author']] = fixed_df.tolist()
-    
-    df = pd.concat([books_filtered, new_sort])
 
-    # Преобразование и очистка некорректных дат в столбце "Year-Of-Publication"
-    df['Year-Of-Publication'] = pd.to_datetime(df['Year-Of-Publication'], errors='coerce')
-    df = df.dropna(subset=['Year-Of-Publication'])
-    
+    # Применяем функцию исправления данных
+    new_sort = df[~mask]
+    fixed_df = new_sort.apply(lambda x: fix_merged_columns(x), axis=1)
+    new_sort['Book-Author'], new_sort['Book-Title'] = zip(*fixed_df)
+
+    df = pd.concat([books_filtered, new_sort])
     # Удаляем строки с будущими годами публикации
-    current_year = pd.to_datetime('today').year
-    df = df[df['Year-Of-Publication'].dt.year <= current_year]  # Сравниваем только год
-    
+    current_year = pd.Timestamp.now().year
+    df['Year-Of-Publication'] = pd.to_numeric(df['Year-Of-Publication'], errors='coerce')
+
+    df.loc[(df['Year-Of-Publication'] > current_year), 'Year-Of-Publication'] = 2024
     # Удаление ненужных столбцов с изображениями
     df = df.drop(columns=['Image-URL-S', 'Image-URL-M', 'Image-URL-L'])
 
@@ -49,6 +54,8 @@ def books_preprocessing(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
+
+
 def ratings_preprocessing(df: pd.DataFrame) -> pd.DataFrame:
     """Функция для предобработки таблицы Ratings.scv
     Целевой переменной в этой задаче будет средний рейтинг книги,
@@ -56,27 +63,27 @@ def ratings_preprocessing(df: pd.DataFrame) -> pd.DataFrame:
     1. Замену оценки книги пользователем на среднюю оценку книги всеми пользователями.
     2. Расчет числа оценок для каждой книги (опционально)."""
     df = df.copy()
-    
+
     # Удаление нулевых рейтингов
     df = df[df['Book-Rating'] != 0]
-    # Задаем минимально кол-во 
+    # Задаем минимально кол-во
     min_books_ratings = 2
 
     # Рассчитываем средний рейтинг для каждой книги
     book_avg_ratings = df.groupby('ISBN')['Book-Rating'].mean()
-    
+
     # Заменяем индивидуальные рейтинги на средние для книги
     df['Book-Rating'] = df['ISBN'].map(book_avg_ratings)
-    
+
     # Рассчитываем число оценок для каждой книги
     book_ratings_count = df.groupby('ISBN')['Book-Rating'].count()
-    
+
     # Добавляем количество оценок к исходным данным
     df['Book-Ratings-Count'] = df['ISBN'].map(book_ratings_count)
-    
+
     # Фильтруем книги, у которых меньше min_books_ratings оценок
     df = df[df['Book-Ratings-Count'] >= min_books_ratings]
-    
+
     # Удалим пользователей, которые поставили меньше min_books_ratings оценок
     user_ratings_count = df.groupby('User-ID')['Book-Rating'].count()
     good_users = user_ratings_count[user_ratings_count >= min_books_ratings].index
@@ -110,62 +117,41 @@ def modeling(books: pd.DataFrame, ratings: pd.DataFrame) -> None:
     5. Обучить и протестировать SGDRegressor
     6. Подобрать гиперпараметры (при необходимости)
     7. Сохранить модель"""
-    # Преобразование авторов и издателей с использованием LabelEncoder
-    author_encoder = LabelEncoder()
-    books['Book-Author'] = author_encoder.fit_transform(books['Book-Author'].fillna('Unknown'))
+    categorical_features = ['Book-Author', 'Publisher']
+    numerical_features = ['Year-Of-Publication']
+    for feature in categorical_features:
+        books[feature] = books[feature].astype('category').cat.codes
+    avg_ratings = ratings[['ISBN', 'Book-Rating']].drop_duplicates()
+    final_df = books.merge(avg_ratings, on='ISBN', how='inner')
+    X = final_df[numerical_features + categorical_features]
+    y = final_df['Book-Rating']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.01, random_state=42)
+    vectorizer = TfidfVectorizer(max_features=1003)
+    titles = final_df['Book-Title']  # .apply(title_preprocessing)
+    X_titles_train = vectorizer.fit_transform(titles.iloc[X_train.index])
+    X_titles_test = vectorizer.transform(titles.iloc[X_test.index])
+    
+    X_train = pd.concat([X_train.reset_index(drop=True), pd.DataFrame(X_titles_train.toarray())], axis=1)
+    X_test = pd.concat([X_test.reset_index(drop=True), pd.DataFrame(X_titles_test.toarray())], axis=1)
+    X_train.columns = X_train.columns.astype(str)
+    X_test.columns = X_test.columns.astype(str)
+    
+    linreg = SGDRegressor()
+    linreg.fit(X_train, y_train)
 
-    publisher_encoder = LabelEncoder()
-    books['Publisher'] = publisher_encoder.fit_transform(books['Publisher'].fillna('Unknown'))
-
-    # Применение TF-IDF для названий книг
-    vectorizer = TfidfVectorizer(max_features=1000)
-    books['Book-Title'] = books['Book-Title'].apply(title_preprocessing)
-    tfidf_matrix = vectorizer.fit_transform(books['Book-Title']).toarray()
-
-    # Объединение данных по ISBN
-    merged_data = ratings.merge(books, on='ISBN')
-    selected_features = ['Book-Author', 'Publisher', 'Year-Of-Publication']
-    X = pd.concat([merged_data[selected_features], pd.DataFrame(tfidf_matrix)], axis=1)
-    y = merged_data['Book-Rating']
-
-    # Загрузка тестового набора для синхронизации признаков
-    test_data = pd.read_csv("linreg_test.csv")
-    test_features = test_data.columns.drop("y")
-
-    X = X.reindex(columns=test_features, fill_value=0)
-
-    # Масштабирование признаков
-    scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(X)
-
-    # Сохранение масштабатора
-    with open("scaler.pkl", "wb") as f:
-        pickle.dump(scaler, f)
-
-    # Разделение на обучающую и тестовую выборки
-    X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
-
-    # Обучение модели линейной регрессии с использованием стохастического градиентного спуска
-    model = SGDRegressor(random_state=42)
-    model.fit(X_train, y_train)
-
-    # Прогнозирование и вычисление MAE
-    y_pred = model.predict(X_test)
-    mae_score = mean_absolute_error(y_test, y_pred)
-    print(f"Средняя абсолютная ошибка (MAE): {mae_score}")
-
-    # Сохранение обученной модели
+    # Тестирование модели и вывод метрик
+    y_pred = linreg.predict(X_test)
+    mae = mean_absolute_error(y_test, y_pred)
+    print(f'MAE: {mae}')
     with open("linreg.pkl", "wb") as file:
-        pickle.dump(model, file)
+        pickle.dump(linreg, file)
 
-if __name__ == "__main__":
-    # Загрузка данных
-    books_data = pd.read_csv("Books.csv", low_memory=False)
-    ratings_data = pd.read_csv("Ratings.csv")
 
-    # Предобработка данных
-    books_data = books_preprocessing(books_data)
-    ratings_data = ratings_preprocessing(ratings_data)
+books1 = pd.read_csv("Books.csv")
+ratings1 = pd.read_csv("Ratings.csv")
+filtered_ratings1 = ratings_preprocessing(ratings1)
+filtered_books1 = books_preprocessing(books1)
 
-    # Запуск модели
-    model_books_ratings(books_data, ratings_data)
+filtered_ratings1.info()
+filtered_books1.info()
+modeling(filtered_books1, filtered_ratings1)
